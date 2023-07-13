d08a08ce437972135d790f14dde580c8c0a6b583
fredyefra <fredweb3@gmail.com>
2023-07-13 00:19:31 -0300

    IMPROVEMENT: Metodo consulta por uf, cidade e logradouro.

--- src/main/java/br/com/consulta/cep/rest/ConsultaCepResource.java
+++ src/main/java/br/com/consulta/cep/rest/ConsultaCepResource.java
@@ -1,16 +1,22 @@
 package br.com.consulta.cep.rest;
 
 import jakarta.inject.Inject;
-import jakarta.ws.rs.*;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
-import org.apache.commons.lang3.StringUtils;
+import org.eclipse.microprofile.opentracing.Traced;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 import org.jboss.resteasy.reactive.RestResponse;
 import org.jboss.resteasy.reactive.server.ServerExceptionMapper;
 
+
+@Traced
 @Path("/ws")
 public class ConsultaCepResource {
+
 
     @Inject
     @RestClient
@@ -21,10 +27,24 @@ public class ConsultaCepResource {
         return RestResponse.status(Response.Status.BAD_REQUEST, "Recurso não localizado: " + negocioException);
     }
 
+
     @GET
     @Path("/{cep}")
     @Produces(MediaType.APPLICATION_JSON)
     public Response consultarCep(@PathParam("cep") final String cep) throws NegocioException {
         return Response.status(Response.Status.OK).entity(service.consultarPorCep(cep)).build();
+    }
+
+    //(UF, Cidade e Logradouro)
+
+
+    @GET
+    @Path("/{uf}/{cidade}/{logradouro}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response consultarPorUFCidadeLogradouro (@PathParam("uf") String uf,
+                                                    @PathParam("cidade") String cidade,
+                                                    @PathParam("logradouro") String logradouro){
+         return Response.status(Response.Status.OK).entity(service.consultarPorUFCidadeLogradouro(uf,cidade,logradouro)).build();
+        //return Response.ok(service.consultarPorUFCidadeLogradouro(uf,cidade,logradouro)).build();
     }
 }
--- src/main/java/br/com/consulta/cep/rest/ConsultaCepService.java
+++ src/main/java/br/com/consulta/cep/rest/ConsultaCepService.java
@@ -5,6 +5,8 @@ import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
+import java.util.List;
+
 @Path(value = "/ws")
 @RegisterRestClient(configKey = "consulta-cep-api")
 public interface ConsultaCepService {
@@ -21,5 +23,14 @@ public interface ConsultaCepService {
     @Produces(MediaType.APPLICATION_JSON)
     @Path("/{cep}/json/")
     EnderecoWrapper consultarPorCep(@PathParam("cep") final String cep);
+
+    //(UF, Cidade e Logradouro)
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/{uf}/{cidade}/{logradouro}/json/")
+    List<EnderecoWrapper> consultarPorUFCidadeLogradouro(
+            @PathParam("uf") String uf,
+            @PathParam("cidade") String cidade,
+            @PathParam("logradouro") String logradouro);
 
 }
