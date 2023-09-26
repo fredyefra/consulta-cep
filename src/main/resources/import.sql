71281bb407f85de7492dad6ead9e055f8b2ee117
fredyefra <fredweb3@gmail.com>
2023-09-26 19:35:04 -0300

    Teste javadoc.

--- src/main/resources/import.sql
+++
@@ -1,6 +0,0 @@
---CREATE SCHEMA IF NOT EXISTS teste AUTHORIZATION sa;
---
-INSERT INTO EnderecoWrapper VALUES (1, '','','','','','','');
-INSERT INTO EnderecoWrapper VALUES (2, '','','','','','','');
-INSERT INTO EnderecoWrapper VALUES (3, '','','','','','','');
-INSERT INTO EnderecoWrapper VALUES (4, '343435','','','','','','');
